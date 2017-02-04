02615a8f7eb5944dfbbc64b7c1e2c7f18d4d3317
Raj Patel <rajpatel@nwk-20-33.nwk.ruw.rutgers.edu>
2017-02-03 21:11:52 -0500

    Added XCode files

---
+++ DoSomething/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoSomething
+//
+//  Created by Raj Patel on 1/28/17.
+//  Copyright © 2017 vrknights. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
